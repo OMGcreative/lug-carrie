4a15d9a6eb88aca38c6f5a93420978f485a43928
OMGcreative <jeremy@omgcreative.com.au>
2022-04-06 15:04:35 +1000

    update onscroll

--- js/add_class_onscroll.js
+++ js/add_class_onscroll.js
@@ -2,12 +2,15 @@
 
 // $.noConflict();
 jQuery(document).ready(function( $ ) {
+  $("#header").addClass("reverse");
     $(window).scroll(function() {
       if ($(this).scrollTop() > 50){
           $("#header").addClass("onscroll");
+          $("#header").removeClass("reverse");
       }
       else{
           $("#header").removeClass("onscroll");
+          $("#header").addClass("reverse");
       }
       if ($(this).scrollTop() > 200){
         $("#header").addClass("shrink");
