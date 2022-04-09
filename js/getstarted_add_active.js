3eb4057791f5b0a7927521a55f6104e48e1b0e36
Jeremy Nicholson <40289122+40ftslinky@users.noreply.github.com>
2022-04-09 18:48:31 +1000

    update js

---
+++ js/getstarted_add_active.js
@@ -0,0 +1,21 @@
+// add_active .js
+
+// $.noConflict();
+jQuery(document).ready(function( $ ) {
+    // $("#getstarted").removeClass("active");
+    // $(".fullscreen").removeClass("active");
+    // $(".fullscreen").hide();
+    $("#getstarted").click(function(e){
+        e.preventDefault()
+        //
+        $(this).toggleClass("active");
+        $(".fullscreen").toggleClass("active");
+        $("body").toggleClass("no_scroll");        
+        //
+        if ($( this ).hasClass( "active ") ) {
+          $( this ).innerHTML = "Close";
+        } else {
+          $( this ).innerHTML = "Get Started";
+        }
+    });
+});
