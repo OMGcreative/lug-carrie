177509f6a4c82be026081911e3ad69b276f82262
OMGcreative <jeremy@omgcreative.com.au>
2022-04-27 13:19:16 +1000

    Add files via upload

--- js/getstarted_add_active.js
+++ js/getstarted_add_active.js
@@ -5,28 +5,27 @@ jQuery(document).ready(function( $ ) {
     // $("#getstarted").removeClass("active");
     // $(".fullscreen").removeClass("active");
     // $(".fullscreen").hide();
-    $( "#getstarted" ).click(function(e){
-        e.preventDefault()
-        //
-        $(this).toggleClass( "active" );
-        $( ".fullscreen" ).toggleClass( "active" );
-        $( "body" ).toggleClass( "no_scroll" );
-        // $("#header").toggleClass("reverse");
-        
-        //
-        if ($( this ).hasClass( "active ") ) {
-          $( this ).innerHTML = "Close";
-          // $("#header").addClass("reverse");
-        } 
-        else if ($(this).scrollTop() > 200){
-          // $("#header").removeClass("reverse");
-        } 
-        else if ($( "#header" ).hasClass( "reverse ") ) {    
-          $("#header").removeClass("reverse");
-        } 
-        else {
-          $( this ).innerHTML = "Get Started";
-          $("#header").removeClass("reverse");
+    $( ".getstarted" ).click(function(e){
+      e.preventDefault()
+      //
+      $(this).toggleClass( "active" );
+      $( ".fullscreen" ).toggleClass( "active" );
+      $( "body" ).toggleClass( "no_scroll" );
+      // $("#header").toggleClass("reverse");
+      //
+      if ($( this ).hasClass( "active") ) {
+        $( this ).html("Close");
+        $("#header").removeClass("reverse");
+      } 
+      else if ($( "#header" ).hasClass( "reverse") ) {    
+        $( this ).html("Get Started");
+        $("#header").removeClass("reverse");
+      } 
+      else {
+        $( this ).html("Get Started");
+        if(!$("#header").hasClass("shrink")){
+          $("#header").addClass("reverse");
         }
-    });
+      }
+  });
 });
